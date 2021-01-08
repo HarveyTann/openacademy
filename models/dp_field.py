a541c5ae0bb24b2323976e4104e71f32f41612c6
HarveyTann <harvey@ozbgroup.com>
2021-01-08 15:44:49 +0800

    use decimal_precision

--- models/dp_field.py
+++ models/dp_field.py
@@ -1,7 +1,11 @@
 from odoo import models, fields
+from odoo.addons import decimal_precision as dp
 
 class DecimalPoints(models.Model):
 
     _inherit = "openacademy.course"
     # digits - 6 before the . 5 decimal points
-    price = fields.Float(string="Course price", digits=(6,5))
+    # price = fields.Float(string="Course Price", digits=(6,5))
+
+    #dp.get_precision
+    price = fields.Float(String="Course Price", digits=dp.get_precision('Course Price'), default=0.0)
